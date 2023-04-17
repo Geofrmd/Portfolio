88afe94be197d4cc8f288f377483000682ca2639
geoffroy moreaud <g.moreaud@gmail.com>
2023-04-17 18:18:19 +0200

    contact form test en prod

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,27 +1,4 @@
 class ContactsController < ApplicationController
-  # def new
-  #   @contact = Contact.new
-  # end
-
-  # def create
-  #   @contact = Contact.new(contact_params)
-
-
-  #   if @contact.save
-  #     flash.now[:success] = 'Message sent!'
-  #     redirect_to root_path
-  #   else
-  #     flash.now[:error] = 'Could not send message'
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
-
-  # private
-
-  # def contact_params
-  #   params.require(:contact).permit(:name, :entreprise, :email, :phone, :message)
-  # end
-
   def new
     @contact = Contact.new
   end
